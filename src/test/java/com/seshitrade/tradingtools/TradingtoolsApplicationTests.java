2201378400ac0e30ca551447bd47723418e1e0ab
seshi jonnala <seshi.jonnala@gmail.com>
2025-03-25 12:14:53 +0530

    init

---
+++ src/test/java/com/seshitrade/tradingtools/TradingtoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seshitrade.tradingtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingtoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
